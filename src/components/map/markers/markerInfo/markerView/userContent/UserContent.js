b81e1e11eb08b72a8b43a068d2cbdbec6f602202
wladyDe <vovawestland@gmail.com>
2023-11-29 14:08:57 +0100

    add follow and unfollow btns

--- src/components/map/markers/markerInfo/markerView/userContent/UserContent.js
+++ src/components/map/markers/markerInfo/markerView/userContent/UserContent.js
@@ -1,13 +1,15 @@
-import { useEffect, useState } from 'react';
-import { getUserInfo } from '../../../../../../service/SocialService';
+import { useEffect, useState, useContext } from 'react';
+import { getUserInfo, followUser, unFollowUser } from '../../../../../../service/SocialService';
 import Spinner from '../../../../../spinner/Spinner';
 import backImg from '../../../../../../ressources/img/back.png';
+import { AuthContext } from '../../../../../../context/AuthContext';
 import './userContent.scss';
 import { animateNumber } from '../../../../../../utils/animation'
 
 const UserContent = ({ view: userId, setView }) => {
     const [user, setUser] = useState(null);
-
+    const [social, setSocial] = useState('nothing');
+    const { currentUser } = useContext(AuthContext);
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -27,9 +29,51 @@ const UserContent = ({ view: userId, setView }) => {
         }
     }, [user]);
 
+    useEffect(() => {
+        if (currentUser.uid === userId) {
+            setSocial('nothing');
+            return;
+        }
+        
+        if (user) {
+            const { followers } = user;
+            if (followers) {
+                const isFollowing = followers.some(user => user === currentUser.uid);
+                if (isFollowing) {
+                    setSocial('unfollow');
+                } else {
+                    setSocial('follow');
+                }
+            } else {
+                setSocial('follow');
+            }
+        }
+    }, [user]);
+
     const backToTheMarker = (e) => {
         e.stopPropagation();
         setView('content');
+    }
+
+    const onFollow = (userId, currentUserId) => {
+        followUser(userId, currentUserId);
+        setSocial('unfollow');
+    }
+
+    const onUnFollow = (userId, currentUserId) => {
+        unFollowUser(userId, currentUserId);
+        setSocial('follow');
+    }
+
+    const getSocial = () => {
+        switch (social) {
+            case 'follow':
+                return <button className='follow follow-btn' onClick={() => onFollow(userId, currentUser.uid)}>Follow</button>
+            case 'unfollow':
+                return <button className='follow unfollow-btn' onClick={() => onUnFollow(userId, currentUser.uid)}>Unfollow</button>
+            default:
+                return null;
+        }
     }
 
     if (!user) {
@@ -42,7 +86,10 @@ const UserContent = ({ view: userId, setView }) => {
             <div className='marker__user'>
                 <img src={user.photoURL} className='marker__user-img' alt="user-photo" />
                 <div className="marker__user-content">
-                    <div className="marker__user-title">{user.displayName}</div>
+                    <div className="marker__user-main">
+                        <div className="marker__user-title">{user.displayName}</div>
+                        {getSocial()}
+                    </div>
                     <div className="marker__user-statistik">
                         <div className="marker__user-social">
                             <span id='marker__user-trainings'></span>
--- src/service/SocialService.js
+++ src/service/SocialService.js
@@ -1,12 +1,72 @@
 import {
     doc,
-    getDoc
+    getDoc, 
+    updateDoc
 } from "firebase/firestore";
 import { db } from "../config/firebase";
 
 export const getUserInfo = async (userId) => {
     const docRef = doc(db, "users", userId);
     const docSnap = await getDoc(docRef);
-    return docSnap.data(); 
+    return docSnap.data();
 }
+
+export const followUser = async (userId, currentUserId) => {
+    const { user, currentUser, docRefUser, docRefCurrentUser } = await getUsers(userId, currentUserId);git 
+
+    const updatedUser = {
+        ...user,
+        followers: [...(user.followers || []), currentUserId]
+    };
+
+    const updatedCurrentUser = {
+        ...currentUser,
+        following: [...(currentUser.following || []), userId]
+    };
+
+    await Promise.all([
+        updateDoc(docRefUser, updatedUser),
+        updateDoc(docRefCurrentUser, updatedCurrentUser)
+    ]);
+}
+
+export const unFollowUser = async (userId, currentUserId) => {
+    const { user, currentUser, docRefUser, docRefCurrentUser } = await getUsers(userId, currentUserId);
+
+    const newFollowers = user.followers.filter(followerId => followerId !== currentUserId);
+    const newFollowing = currentUser.following.filter(followedId => followedId !== userId);
+
+    const updatedUser = {
+        ...user,
+        followers: newFollowers
+    };
+
+    const updatedCurrentUser = {
+        ...currentUser,
+        following: newFollowing
+    };
+
+    await Promise.all([
+        updateDoc(docRefUser, updatedUser),
+        updateDoc(docRefCurrentUser, updatedCurrentUser)
+    ]);
+}
+
+const getUsers = async (userId, currentUserId) => {
+    const docRefUser = doc(db, "users", userId);
+    const docRefCurrentUser = doc(db, "users", currentUserId);
+
+    const [docSnapUser, docSnapCurrentUser] = await Promise.all([
+        getDoc(docRefUser),
+        getDoc(docRefCurrentUser)
+    ]);
+
+    return {
+        user: docSnapUser.data(),
+        currentUser: docSnapCurrentUser.data(),
+        docRefUser, 
+        docRefCurrentUser
+    }
+}
+
 
